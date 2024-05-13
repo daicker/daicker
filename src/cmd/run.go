31dd1e404d9537f9f8b7988c0cc03f620614453b
Toru Ikeda <toru48@outlook.jp>
2024-05-13 20:31:53 +0900

    add run command logic

--- src/cmd/run.go
+++ src/cmd/run.go
@@ -1,36 +1,54 @@
 package cmd
 
 import (
+	"errors"
 	"fmt"
+	"os"
 
+	"github.com/daicker/daicker/src/lang"
 	"github.com/spf13/cobra"
 )
 
 // runCmd represents the run command
 var runCmd = &cobra.Command{
 	Use:   "run",
-	Short: "A brief description of your command",
-	Long: `A longer description that spans multiple lines and likely contains examples
-and usage of using your command. For example:
-
-Cobra is a CLI library for Go that empowers applications.
-This application is a tool to generate the needed files
-to quickly create a Cobra application.`,
+	Short: "Run function",
+	Long:  `Run function`,
+	Args: func(cmd *cobra.Command, args []string) error {
+		if len(args) < 1 {
+			return errors.New("requires file name")
+		}
+		return nil
+	},
 	Run: func(cmd *cobra.Command, args []string) {
-		fmt.Println("run called")
+		file := args[0]
+		src, err := os.ReadFile(file)
+		if err != nil {
+			panic(err)
+		}
+
+		tokens, err := lang.Lex(string(src))
+		if err != nil {
+			panic(err)
+		}
+
+		mod, err := lang.Parse(tokens)
+		if err != nil {
+			panic(err)
+		}
+
+		res, err := lang.Eval(function, mod)
+		if err != nil {
+			panic(err)
+		}
+
+		fmt.Println(res)
 	},
 }
 
+var function string
+
 func init() {
 	rootCmd.AddCommand(runCmd)
-
-	// Here you will define your flags and configuration settings.
-
-	// Cobra supports Persistent Flags which will work for this command
-	// and all subcommands, e.g.:
-	// runCmd.PersistentFlags().String("foo", "", "A help for foo")
-
-	// Cobra supports local flags which will only run when this command
-	// is called directly, e.g.:
-	// runCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
+	runCmd.Flags().StringVarP(&function, "function", "f", "main", "Daicker target function")
 }
