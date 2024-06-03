6ec3e314a75671b2b732f071c785ec2d9634aac6
Toru Ikeda <toru48@outlook.jp>
2024-06-03 23:49:09 +0900

    add skeleton language server code

--- src/Language/Daicker/DLS.hs
+++ src/Language/Daicker/DLS.hs
@@ -1,57 +1,136 @@
+{-# LANGUAGE DataKinds #-}
 {-# LANGUAGE DuplicateRecordFields #-}
-{-# LANGUAGE LambdaCase #-}
+{-# LANGUAGE GADTs #-}
 {-# LANGUAGE OverloadedStrings #-}
+{-# LANGUAGE RankNTypes #-}
+{-# LANGUAGE KindSignatures #-}
+{-# LANGUAGE DeriveGeneric #-}
+{-# LANGUAGE TypeOperators #-}
 
 module Language.Daicker.DLS where
 
+import Colog.Core (LogAction (..), Severity (..), WithSeverity (..), (<&))
+import qualified Colog.Core as L
 import Control.Monad.IO.Class
+import Control.Lens hiding (Iso)
+import Control.Concurrent
+import Control.Concurrent.STM.TChan
 import Data.Text as T
-import Language.LSP.Protocol.Message
+import qualified Data.Aeson as J
+import Language.LSP.Protocol.Message ()
 import Language.LSP.Protocol.Types
 import Language.LSP.Server
+import GHC.Generics (Generic)
+import qualified Language.LSP.Protocol.Lens as LSP
+import Language.LSP.Logging (defaultClientLogger)
+import qualified Language.LSP.Protocol.Message as LSP
+import qualified Language.LSP.Protocol.Types as LSP
+import Language.LSP.Diagnostics
+import Control.Concurrent.STM
+import Control.Monad (forever)
 
-handlers :: Handlers (LspM ())
-handlers =
+data Config = Config {fooTheBar :: Bool, wibbleFactor :: Int}
+  deriving (Generic, Show)
+
+instance J.ToJSON Config where
+  toEncoding = J.genericToEncoding J.defaultOptions
+
+instance J.FromJSON Config where
+
+sendDiagnostics :: LSP.NormalizedUri -> Maybe Int32 -> LspM Config ()
+sendDiagnostics fileUri version = do
+  let
+    diags =
+      [ LSP.Diagnostic
+          (LSP.Range (LSP.Position 0 1) (LSP.Position 0 5))
+          (Just LSP.DiagnosticSeverity_Warning) -- severity
+          Nothing -- code
+          Nothing
+          (Just "lsp-hello") -- source
+          "Example diagnostic message"
+          Nothing -- tags
+          (Just [])
+          Nothing
+      ]
+  publishDiagnostics 100 fileUri version (partitionBySource diags)
+
+handle :: (m ~ LspM Config) => L.LogAction m (WithSeverity T.Text) -> Handlers m
+handle logger =
   mconcat
-    [ notificationHandler SMethod_Initialized $ \_not -> do
-        let params =
-              ShowMessageRequestParams
-                MessageType_Info
-                "Turn on code lenses?"
-                (Just [MessageActionItem "Turn on", MessageActionItem "Don't"])
-        _ <- sendRequest SMethod_WindowShowMessageRequest params $ \case
-          Right (InL (MessageActionItem "Turn on")) -> do
-            let regOpts = CodeLensRegistrationOptions (InR Null) Nothing (Just False)
-
-            _ <- registerCapability mempty SMethod_TextDocumentCodeLens regOpts $ \_req responder -> do
-              let cmd = Command "Say hello" "lsp-hello-command" Nothing
-                  rsp = [CodeLens (mkRange 0 0 0 100) (Just cmd) Nothing]
-              responder $ Right $ InL rsp
-            pure ()
-          Right _ ->
-            sendNotification SMethod_WindowShowMessage (ShowMessageParams MessageType_Info "Not turning on code lenses")
-          Left err ->
-            sendNotification SMethod_WindowShowMessage (ShowMessageParams MessageType_Error $ "Something went wrong!\n" <> T.pack (show err))
-        pure ()
-    , requestHandler SMethod_TextDocumentHover $ \req responder -> do
-        let TRequestMessage _ _ _ (HoverParams _doc pos _workDone) = req
-            Position _l _c' = pos
-            rsp = Hover (InL ms) (Just range)
-            ms = mkMarkdown "Hello world"
-            range = Range pos pos
-        responder (Right $ InL rsp)
+    [ notificationHandler LSP.SMethod_TextDocumentDidOpen $ \msg -> do
+        let doc = msg ^. LSP.params . LSP.textDocument . LSP.uri
+            fileName = LSP.uriToFilePath doc
+        logger <& ("Processing DidOpenTextDocument for: " <> T.pack (show fileName)) `WithSeverity` Info
+        sendDiagnostics (LSP.toNormalizedUri doc) (Just 0)
+    , notificationHandler LSP.SMethod_TextDocumentDidSave $ \msg -> do
+        let doc = msg ^. LSP.params . LSP.textDocument . LSP.uri
+            fileName = LSP.uriToFilePath doc
+        logger <& ("Processing DidSaveTextDocument  for: " <> T.pack (show fileName)) `WithSeverity` Info
+        sendDiagnostics (LSP.toNormalizedUri doc) Nothing
     ]
 
+newtype ReactorInput
+  = ReactorAction (IO ())
+
 serve :: IO Int
-serve =
+serve = do
+  let
+    stderrLogger :: LogAction IO (WithSeverity T.Text)
+    stderrLogger = L.cmap show L.logStringStderr
+    clientLogger :: LogAction (LspM Config) (WithSeverity T.Text)
+    clientLogger = defaultClientLogger
+    dualLogger :: LogAction (LspM Config) (WithSeverity T.Text)
+    dualLogger = clientLogger <> L.hoistLogAction liftIO stderrLogger
+
+  rin <- atomically newTChan :: IO (TChan ReactorInput)
   runServer $
     ServerDefinition
-      { parseConfig = const $ const $ Right ()
-      , onConfigChange = const $ pure ()
-      , defaultConfig = ()
-      , configSection = "demo"
-      , doInitialize = \env _req -> pure $ Right env
-      , staticHandlers = const handlers
-      , interpretHandler = \env -> Iso (runLspT env) liftIO
-      , options = defaultOptions
-      }
+        { defaultConfig = Config{fooTheBar = False, wibbleFactor = 0}
+        , parseConfig = \_old v -> do
+            case J.fromJSON v of
+              J.Error e -> Left (T.pack e)
+              J.Success cfg -> Right cfg
+        , onConfigChange = const $ pure ()
+        , configSection = "demo"
+        , doInitialize = \env _ -> forkIO (reactor stderrLogger rin) >> pure (Right env)
+        , -- Handlers log to both the client and stderr
+          staticHandlers = \_caps -> lspHandlers dualLogger rin
+        , interpretHandler = \env -> Iso (runLspT env) liftIO
+        , options = lspOptions
+        }
+
+syncOptions :: LSP.TextDocumentSyncOptions
+syncOptions =
+  LSP.TextDocumentSyncOptions
+    { LSP._openClose = Just True
+    , LSP._change = Just LSP.TextDocumentSyncKind_Incremental
+    , LSP._willSave = Just False
+    , LSP._willSaveWaitUntil = Just False
+    , LSP._save = Just $ LSP.InR $ LSP.SaveOptions $ Just False
+    }
+
+lspOptions :: Options
+lspOptions =
+  defaultOptions
+    { optTextDocumentSync = Just syncOptions
+    , optExecuteCommandCommands = Just ["lsp-hello-command"]
+    }
+
+lspHandlers :: (m ~ LspM Config) => L.LogAction m (WithSeverity T.Text) -> TChan ReactorInput -> Handlers m
+lspHandlers logger rin = mapHandlers goReq goNot (handle logger)
+ where
+  goReq :: forall (a :: LSP.Method LSP.ClientToServer LSP.Request). Handler (LspM Config) a -> Handler (LspM Config) a
+  goReq f msg k = do
+    env <- getLspEnv
+    liftIO $ atomically $ writeTChan rin $ ReactorAction (runLspT env $ f msg k)
+  goNot :: forall (a :: LSP.Method LSP.ClientToServer LSP.Notification). Handler (LspM Config) a -> Handler (LspM Config) a
+  goNot f msg = do
+    env <- getLspEnv
+    liftIO $ atomically $ writeTChan rin $ ReactorAction (runLspT env $ f msg)
+
+reactor :: L.LogAction IO (WithSeverity T.Text) -> TChan ReactorInput -> IO ()
+reactor logger inp = do
+  logger <& "Started the reactor" `WithSeverity` Info
+  forever $ do
+    ReactorAction act <- atomically $ readTChan inp
+    act
