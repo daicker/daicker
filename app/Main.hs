8cda70bc7e0f2fbb5d207eb0b6c77df49a7a3126
Toru Ikeda <toru48@outlook.jp>
2024-07-08 21:23:03 +0900

    add argument input

--- app/Main.hs
+++ app/Main.hs
@@ -4,10 +4,12 @@ module Main where
 
 import Control.Comonad.Cofree
 import Control.Monad (join, void)
+import Control.Monad.Writer.Strict
 import Data.Aeson (decode, encode)
 import Data.Aeson.Types (Value)
 import Data.ByteString.Lazy.Char8 (pack, unpack)
-import Language.Daicker.AST (Define' (Define), Expr, Expr' (EFun))
+import Data.Maybe (fromMaybe)
+import Language.Daicker.AST (Define' (Define), Expr, Expr' (EArray, EFun, ENull))
 import Language.Daicker.DLS (serve)
 import Language.Daicker.Executor (execDefine, findDefine)
 import Language.Daicker.Lexer (mkTStream)
@@ -20,8 +22,31 @@ opts :: Parser (IO ())
 opts =
   subparser
     ( command "serve" (info (pure $ void serve) (progDesc "Run daicker language server"))
-        <> command "valid" (info (valid <$> argument str idm) (progDesc "Valid"))
-        <> command "run" (info (run <$> argument str idm <*> argument str idm) (progDesc "Execute"))
+        <> command
+          "valid"
+          ( info
+              (valid <$> fileOpt)
+              (progDesc "Validate daicker file")
+          )
+        <> command
+          "run"
+          ( info
+              ( run
+                  <$> argument str (metavar "FUNCTION")
+                  <*> many (argument str (metavar "ARGS"))
+                  <*> fileOpt
+              )
+              (progDesc "Execute function")
+          )
+    )
+
+fileOpt :: Parser String
+fileOpt =
+  strOption
+    ( long "file"
+        <> short 'f'
+        <> metavar "FILENAME"
+        <> value "main.daic"
     )
 
 valid :: String -> IO ()
@@ -34,10 +59,8 @@ valid fileName = do
         Right m -> pure ()
         Left e -> putStrLn $ errorBundlePretty e
 
-run :: String -> String -> IO ()
-run fileName funcName = do
-  input <- getContents
-  let arg = decode (pack input) :: Maybe (Expr ())
+run :: String -> [String] -> String -> IO ()
+run funcName args fileName = do
   src <- readFile fileName
   case mkTStream fileName src of
     Left e -> putStrLn $ errorBundlePretty e
@@ -46,9 +69,18 @@ run fileName funcName = do
       Right m -> case findDefine funcName m of
         Nothing -> putStrLn $ "not defined: " <> funcName
         -- Requires an argument
-        Just d@(_ :< Define _ (_ :< EFun {})) -> case execDefine m d arg of
-          Left e -> print e
-          Right e -> putStrLn (unpack $ encode e)
+        Just d@(_ :< Define _ (_ :< EFun (Just _) _)) -> do
+          arg <- case args of
+            [] -> do
+              input <- getContents
+              pure (decode (pack input) :: Maybe (Expr ()))
+            [arg] -> do
+              pure (decode (pack arg) :: Maybe (Expr ()))
+            args -> do
+              pure $ (:<) () . EArray <$> mapM (\arg -> decode (pack arg) :: Maybe (Expr ())) args
+          case execDefine m d arg of
+            Left e -> print e
+            Right e -> putStrLn (unpack $ encode e)
         -- No argument
         Just d -> case execDefine m d Nothing of
           Left e -> print e
