ccd719f07b5057b9c3a0a904b69ae4c6065ca1a6
Toru Ikeda <toru48@outlook.jp>
2024-10-17 00:27:30 +0900

    add object parser

--- src/Language/Daicker/Parser.hs
+++ src/Language/Daicker/Parser.hs
@@ -6,6 +6,8 @@ import Control.Comonad.Cofree
 import Control.Lens (op)
 import Control.Monad (void)
 import Control.Monad.Combinators.Expr (Operator (..), makeExprParser)
+import Control.Monad.State (StateT)
+import Control.Monad.State.Class (get, put)
 import Data.Functor.Identity (Identity)
 import Data.List.NonEmpty (NonEmpty (..), some1)
 import Data.Maybe (fromJust, fromMaybe, isJust)
@@ -35,21 +37,8 @@ import Text.Megaparsec.Char
     upperChar,
   )
 import qualified Text.Megaparsec.Char.Lexer as L
-import Text.ParserCombinators.ReadP (many1)
 
-data MetaNode = MetaNode
-  { nodeSpan :: Span,
-    nodeMetaTokens :: [MetaToken]
-  }
-  deriving (Show, Eq)
-
-data Token a = Token
-  { tokenMeta :: MetaToken,
-    tokenValue :: a
-  }
-  deriving (Show, Eq)
-
-data MetaToken = MetaToken
+data Token = Token
   { tokenKind :: TokenKind,
     tokenSpan :: Span
   }
@@ -63,12 +52,13 @@ data TokenKind
   | TKKeyword
   | TKNumber
   | TKString
+  | TKSep
   | TKComment
   deriving (Show, Eq)
 
-type Parser = Parsec Void Text
+type Parser = StateT [Token] (Parsec Void Text)
 
-opTable :: [[Operator Parser (Expr MetaNode)]]
+opTable :: [[Operator Parser (Expr Span)]]
 opTable =
   [ [ binary "*",
       binary "/"
@@ -78,60 +68,80 @@ opTable =
     ]
   ]
 
-binary :: Text -> Operator Parser (Expr MetaNode)
+binary :: Text -> Operator Parser (Expr Span)
 binary name = do
   InfixL (f <$> pOp name)
   where
-    f :: Expr MetaNode -> Expr MetaNode -> Expr MetaNode -> Expr MetaNode
-    f op@(op' :< _) a@(a' :< _) b@(b' :< _) =
-      MetaNode (nodeSpan a' `union` nodeSpan b') (concatMap nodeMetaTokens [a', op', b'])
+    f :: Expr Span -> Expr Span -> Expr Span -> Expr Span
+    f op a@(a' :< _) b@(b' :< _) =
+      (a' `union` b')
         :< EApp Nothing op [(a, False), (b, False)]
 
-pOp :: Text -> Parser (Expr MetaNode)
+pOp :: Text -> Parser (Expr Span)
 pOp name = do
-  Token m op <- tOp name
-  pure $ singleTokenMetaNode m :< ERef (singleTokenMetaNode m :< Identifier op)
+  (s, op) <- spanned $ tOp name
+  pure $ s :< ERef (s :< Identifier op)
 
-pExpr :: Parser (Expr MetaNode)
+pExpr :: Parser (Expr Span)
 pExpr = makeExprParser pTerm opTable
 
-pTerm :: Parser (Expr MetaNode)
-pTerm = choice [pString, pNumber, pBool, pNull]
+pTerm :: Parser (Expr Span)
+pTerm =
+  choice
+    [ pObject,
+      pArray,
+      pString,
+      pNumber,
+      pBool,
+      pNull
+    ]
 
-pArray :: Parser (Expr MetaNode)
-pArray = do
-  (Token s _) <- lexeme $ token TKOp $ char '['
-  elements <- pExpr `sepBy` lexeme (char ',') -- TODO: Add trailing comma support and collect comma tokens.
-  let elementTokens = concatMap (\(m :< _) -> nodeMetaTokens m) elements
-  (Token e _) <- lexeme $ token TKOp $ char ']'
-  pure $
-    MetaNode
-      (tokenSpan s `union` tokenSpan e)
-      (concat [[s], elementTokens, [e]])
-      :< EArray elements
+pVar :: Parser (Expr Span)
+pVar = lexeme $ tupleToCofree EVar <$> spanned (tExprIdentifier TKVar)
 
-pString :: Parser (Expr MetaNode)
-pString = lexeme $ singleTokenNode <$> tString <*> pure EString
+pObject :: Parser (Expr Span)
+pObject = do
+  tupleToCofree EObject
+    <$> spanned
+      ( between
+          (lexeme $ token TKSep $ char '{')
+          (lexeme $ token TKSep $ char '}')
+          (pPair `sepBy` lexeme (token TKSep (char ',')))
+      )
+  where
+    pPair :: Parser (Expr Span, Expr Span)
+    pPair = do
+      key <- pExpr
+      _ <- lexeme $ token TKOp $ char ':'
+      value <- pExpr
+      pure (key, value)
 
-pNumber :: Parser (Expr MetaNode)
-pNumber = lexeme $ singleTokenNode <$> tNumber <*> pure ENumber
+pArray :: Parser (Expr Span)
+pArray =
+  tupleToCofree EArray
+    <$> spanned
+      ( between
+          (lexeme $ token TKSep $ char '[')
+          (lexeme $ token TKSep $ char ']')
+          (pExpr `sepBy` lexeme (token TKSep (char ','))) -- TODO: Add trailing comma support.
+      )
 
-pBool :: Parser (Expr MetaNode)
-pBool = lexeme $ singleTokenNode <$> tBool <*> pure EBool
+pString :: Parser (Expr Span)
+pString = lexeme $ tupleToCofree EString <$> spanned tString
 
-pNull :: Parser (Expr MetaNode)
-pNull = lexeme $ (:< ENull) . singleTokenMetaNode . tokenMeta <$> tNull
+pNumber :: Parser (Expr Span)
+pNumber = lexeme $ tupleToCofree ENumber <$> spanned tNumber
 
-singleTokenNode ::
-  Token t ->
-  (t -> f (Cofree f MetaNode)) ->
-  Cofree f MetaNode
-singleTokenNode (Token m v) f = singleTokenMetaNode m :< f v
+pBool :: Parser (Expr Span)
+pBool = lexeme $ tupleToCofree EBool <$> spanned tBool
 
-singleTokenMetaNode :: MetaToken -> MetaNode
-singleTokenMetaNode m = MetaNode (tokenSpan m) [m]
+pNull :: Parser (Expr Span)
+pNull = lexeme $ (:< ENull) . fst <$> spanned tNull
 
-tExprIdentifier :: TokenKind -> Parser (Token String)
+tupleToCofree :: (t -> f (Cofree f a)) -> (a, t) -> Cofree f a
+tupleToCofree f (s, v) = s :< f v
+
+tExprIdentifier :: TokenKind -> Parser String
 tExprIdentifier kind =
   token
     kind
@@ -140,25 +150,34 @@ tExprIdentifier kind =
         <*> many (alphaNumChar <|> char '_' <|> char '-')
     )
 
-tOp :: Text -> Parser (Token String)
+tOp :: Text -> Parser String
 tOp name = token TKOp (T.unpack <$> string name)
 
-tString :: Parser (Token String)
+tString :: Parser String
 tString = token TKString $ char '"' *> manyTill L.charLiteral (char '"')
 
-tNumber :: Parser (Token Scientific)
+tNumber :: Parser Scientific
 tNumber = token TKNumber $ L.signed sc L.scientific
 
-tBool :: Parser (Token Bool)
+tBool :: Parser Bool
 tBool = token TKKeyword $ (True <$ keyword "true") <|> (False <$ keyword "false")
 
-tNull :: Parser (Token ())
+tNull :: Parser ()
 tNull = token TKKeyword $ keyword "null"
 
-token :: TokenKind -> Parser a -> Parser (Token a)
+token :: TokenKind -> Parser a -> Parser a
 token kind p = do
-  (s, v) <- spanned p
-  pure $ Token (MetaToken kind s) v
+  (s, v) <- tokenSpanned p
+  tokens <- get
+  put $ tokens <> [Token kind s]
+  pure v
+  where
+    tokenSpanned :: Parser a -> Parser (Span, a)
+    tokenSpanned p = do
+      start <- getSourcePos
+      x <- p
+      end <- getSourcePos
+      pure (Span (S.fromSourcePos start) (S.fromSourcePos end), x)
 
 keyword :: Text -> Parser ()
 keyword k = try $ void (string k) <* notFollowedBy alphaNumChar
@@ -173,11 +192,12 @@ lineComment :: Parser ()
 lineComment = L.skipLineComment "//"
 
 blockComment :: Parser ()
-blockComment = L.skipBlockComment "/*" "*/"
+blockComment = token TKComment $ L.skipBlockComment "/*" "*/"
 
 spanned :: Parser a -> Parser (Span, a)
 spanned p = do
-  start <- getSourcePos
+  start <- length <$> get
   x <- p
-  end <- getSourcePos
-  pure (Span (S.fromSourcePos start) (S.fromSourcePos end), x)
+  end <- length <$> get
+  tokens <- take (end - start) . drop start <$> get
+  pure (tokenSpan (head tokens) `union` tokenSpan (last tokens), x)
