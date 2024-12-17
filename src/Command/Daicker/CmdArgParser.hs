b17ca3270b02d75421e716c18c8c7f47c0ed5556
Toru Ikeda <toru48@outlook.jp>
2024-12-18 00:05:18 +0900

    refactor

--- src/Command/Daicker/CmdArgParser.hs
+++ src/Command/Daicker/CmdArgParser.hs
@@ -10,52 +10,45 @@ import Data.Text (Text)
 import Data.Void (Void)
 import Language.Daicker.AST
 import Language.Daicker.Error (StaticError (StaticError), fromParseErrorBundle)
+import Language.Daicker.Parser (Parser, TokenKind (..), lexeme, pBool, pNull, pNumber, pString, parse, spanned, tLowerIdentifier, token, tupleToCofree)
 import Language.Daicker.Span
 import Text.Megaparsec
+  ( MonadParsec (try),
+    choice,
+    many,
+    optional,
+  )
 import Text.Megaparsec.Char
 import qualified Text.Megaparsec.Char.Lexer as L
-
-type CmdArgParser = Parsec Void Text
 
 parseArg :: String -> Maybe ByteString -> Text -> Either [StaticError Span] (Argument Span)
 parseArg fileName stdinContent src =
   case parse (pArgument stdinContent) fileName src of
-    Right e -> pure e
-    Left e -> Left [fromParseErrorBundle e]
+    Right (e, _) -> pure e
+    Left e -> Left e
 
-pArgument :: Maybe ByteString -> CmdArgParser (Argument Span)
+pArgument :: Maybe ByteString -> Parser (Argument Span)
 pArgument stdinContent =
   choice
     [ try keywordArgument,
       positionedArgument
     ]
   where
-    positionedArgument :: CmdArgParser (Argument Span)
+    positionedArgument :: Parser (Argument Span)
     positionedArgument = do
-      (WithSpan arg s) <- withSpan $ do
+      (s, arg) <- spanned $ do
         v <- pExpr stdinContent
-        isExpanded <- isJust <$> optional (string "...")
+        isExpanded <- isJust <$> optional (lexeme $ token TKOp $ string "...")
         pure $ PositionedArgument isExpanded v
       pure $ s :< arg
-    keywordArgument :: CmdArgParser (Argument Span)
+    keywordArgument :: Parser (Argument Span)
     keywordArgument = do
-      (WithSpan arg s) <- withSpan $ do
-        i <- pIdentifier
-        _ <- lexeme $ char '='
-        v <- pExpr stdinContent
-        pure $ KeywordArgument i v
-      pure $ s :< arg
+      key@(s1 :< _) <- tupleToCofree Identifier <$> lexeme (spanned (tLowerIdentifier TKParameter))
+      _ <- lexeme $ token TKSep $ char '='
+      (s2, value) <- spanned $ pExpr stdinContent
+      pure $ s1 `union` s2 :< KeywordArgument key value
 
-pIdentifier :: CmdArgParser (Identifier Span)
-pIdentifier = do
-  (WithSpan t s) <-
-    withSpan $
-      (:)
-        <$> (lowerChar <|> upperChar <|> char '_')
-        <*> many (alphaNumChar <|> char '_' <|> char '-')
-  return $ s :< Identifier t
-
-pExpr :: Maybe ByteString -> CmdArgParser (Expr Span)
+pExpr :: Maybe ByteString -> Parser (Expr Span)
 pExpr content =
   lexeme $
     choice
@@ -67,29 +60,9 @@ pExpr content =
         pImplicitString
       ]
 
-pNull :: CmdArgParser (Expr Span)
-pNull = do
-  (WithSpan _ s) <- withSpan $ string "null"
-  return $ s :< ENull
-
-pBool :: CmdArgParser (Expr Span)
-pBool = do
-  (WithSpan b s) <-
-    withSpan $
-      choice
-        [ False <$ string "false",
-          True <$ string "true"
-        ]
-  return $ s :< EBool b
-
-pNumber :: CmdArgParser (Expr Span)
-pNumber = try $ do
-  (WithSpan n s) <- withSpan $ L.signed sc L.scientific
-  return $ s :< ENumber n
-
-pStdin :: Maybe ByteString -> CmdArgParser (Expr Span)
+pStdin :: Maybe ByteString -> Parser (Expr Span)
 pStdin content = do
-  (WithSpan _ s) <- withSpan $ string "@stdin"
+  (s, _) <- spanned $ string "@stdin"
   content <- case content of
     Nothing -> fail "stdin is not provided"
     Just content -> pure content
@@ -98,25 +71,7 @@ pStdin content = do
     Just expr -> pure expr
   return $ switchAnn (const s) expr
 
-pString :: CmdArgParser (Expr Span)
-pString = do
-  (WithSpan t s) <- withSpan $ char '"' *> manyTill L.charLiteral (char '"')
-  return $ s :< EString t
-
-pImplicitString :: CmdArgParser (Expr Span)
+pImplicitString :: Parser (Expr Span)
 pImplicitString = do
-  (WithSpan t s) <- withSpan $ many L.charLiteral
+  (s, t) <- spanned $ many L.charLiteral
   return $ s :< EString t
-
-lexeme :: CmdArgParser a -> CmdArgParser a
-lexeme = L.lexeme sc
-
-sc :: CmdArgParser ()
-sc = L.space space1 empty empty
-
-withSpan :: CmdArgParser a -> CmdArgParser (WithSpan a)
-withSpan lexer = do
-  start@(SourcePos _ _ c1) <- getSourcePos
-  x <- lexer
-  end@(SourcePos _ _ c2) <- getSourcePos
-  pure $ WithSpan x $ Span (fromSourcePos start) (fromSourcePos end)
