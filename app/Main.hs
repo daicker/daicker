cad7900acfeff1d3268b6e50c1fe33ec59e8b661
Toru Ikeda <toru48@outlook.jp>
2024-12-18 21:38:44 +0900

    refactor

--- app/Main.hs
+++ app/Main.hs
@@ -1,86 +1,9 @@
-{-# LANGUAGE OverloadedStrings #-}
-
 module Main where
 
-import Command.Daicker.Entry (hExitWithCodeErrors, hExitWithExpr, withDevNull)
-import qualified Command.Daicker.Entry as E
-import Control.Monad (join, void)
-import Control.Monad.Except (runExceptT)
-import Data.Aeson (decode, encode)
-import Data.Aeson.Types (Value)
-import Data.ByteString.Lazy.Char8 (pack, unpack)
-import Data.Text (Text)
-import GHC.IO.IOMode (IOMode (..))
-import GHC.IO.StdHandles (openFile)
-import Language.Daicker.Error (staticErrorListPretty)
-import Language.Daicker.Parser (pModule, parse)
-import Language.LSP.Daicker (serve)
+import Command.Daicker.Entry (opts)
+import Control.Monad (join)
 import Options.Applicative
-import System.IO (hClose, hGetContents, hIsClosed, hIsOpen, hPutStrLn, hReady, hWaitForInput, stderr, stdin, stdout)
-import System.IO.Error.Lens (fileName)
-import Text.Megaparsec (parse, parseErrorPretty)
-
-opts :: Parser (IO ())
-opts =
-  subparser
-    ( command "serve" (info (pure $ void serve) (progDesc "Runs daicker language server."))
-        <> command
-          "check"
-          ( info
-              (check <$> fileOpt)
-              (progDesc "Validates daicker file")
-          )
-        <> command
-          "run"
-          ( info
-              ( run
-                  <$> fileOpt
-                  <*> argument str (metavar "FUNCTION")
-                  <*> many (argument str (metavar "ARGS"))
-              )
-              (progDesc "Runs the specific function." <> noIntersperse)
-          )
-        <> command
-          "eval"
-          ( info
-              ( eval
-                  <$> fileOpt
-                  <*> argument str (metavar "FUNCTION")
-                  <*> many (argument str (metavar "ARGS"))
-              )
-              (progDesc "Evaluates the specific function and prints the return value to stdout." <> noIntersperse)
-          )
-    )
-
-fileOpt :: Parser String
-fileOpt =
-  strOption
-    ( long "file"
-        <> short 'f'
-        <> metavar "FILENAME"
-        <> value "main.daic"
-    )
-
-check :: String -> IO ()
-check fileName = do
-  res <- runExceptT $ E.validate fileName
-  case res of
-    Right _ -> hPutStrLn stderr "The module is valid!"
-    Left es -> hPutStrLn stderr $ staticErrorListPretty es
-
-run :: String -> String -> [Text] -> IO ()
-run fileName funcName args = do
-  res <- runExceptT (E.run fileName funcName args)
-  case res of
-    Left e -> hExitWithCodeErrors stderr e
-    Right e -> withDevNull (`hExitWithExpr` e)
-
-eval :: String -> String -> [Text] -> IO ()
-eval fileName funcName args = do
-  res <- runExceptT (E.run fileName funcName args)
-  case res of
-    Left e -> hExitWithCodeErrors stderr e
-    Right e -> hExitWithExpr stdout e
+import Options.Applicative.Builder (info)
 
 main :: IO ()
 main = join $ execParser (info (opts <**> helper) idm)
--- src/Command/Daicker/Entry.hs
+++ src/Command/Daicker/Entry.hs
@@ -4,28 +4,97 @@ module Command.Daicker.Entry where
 
 import Command.Daicker.CmdArgParser (parseArg)
 import Control.Comonad.Cofree (Cofree (..))
-import Control.Monad (liftM, void)
+import Control.Monad (join, liftM, void)
 import Control.Monad.Except (ExceptT, MonadError (throwError), liftEither, runExceptT, withExceptT)
 import Control.Monad.IO.Class (liftIO)
 import Control.Monad.Identity (Identity)
-import Data.Aeson (encode)
-import Data.ByteString.Lazy.Char8 (unpack)
+import Data.Aeson (decode, encode)
+import Data.Aeson.Types (Value)
+import Data.ByteString.Lazy.Char8 (pack, unpack)
 import qualified Data.ByteString.Lazy.Char8 as B
 import Data.Sequence (mapWithIndex)
 import Data.Text (Text)
 import qualified Data.Text.IO as T
+import GHC.IO.IOMode (IOMode (..))
+import GHC.IO.StdHandles (openFile)
 import Language.Daicker.AST (Argument, Argument' (..), Expr, Expr' (..), Identifier, Module, Module' (..), Statement' (SExpr), switchAnn)
 import Language.Daicker.Bundler (findExpr, loadEnvironment)
-import Language.Daicker.Error (CodeError (RuntimeE, StaticE), RuntimeError (RuntimeError), StaticError, codeErrorPretty)
-import Language.Daicker.Executor (eval)
+import Language.Daicker.Error (CodeError (RuntimeE, StaticE), RuntimeError (RuntimeError), StaticError, codeErrorPretty, staticErrorListPretty)
+import qualified Language.Daicker.Executor as E
 import Language.Daicker.Parser (pModule, parse)
 import Language.Daicker.Span (Span, mkSpan, spanPretty)
 import Language.Daicker.StdLib (prelude)
-import Language.Daicker.Validator as V
+import qualified Language.Daicker.Validator as V
+import Language.LSP.Daicker (serve)
+import Options.Applicative
 import System.Directory (createDirectoryIfMissing)
 import System.Exit (ExitCode (ExitFailure, ExitSuccess), exitFailure, exitSuccess, exitWith)
 import System.FilePath ((</>))
-import System.IO (Handle, IOMode (..), hClose, hGetContents, hPutStrLn, hReady, openFile, stdin)
+import System.IO (Handle, IOMode (..), hClose, hGetContents, hIsClosed, hIsOpen, hPutStrLn, hReady, hWaitForInput, openFile, stderr, stdin, stdout)
+import System.IO.Error.Lens (fileName)
+import Text.Megaparsec (parseErrorPretty)
+
+opts :: Parser (IO ())
+opts =
+  subparser
+    ( command "serve" (info (pure $ void serve) (progDesc "Runs daicker language server."))
+        <> command
+          "check"
+          ( info
+              (check <$> fileOpt)
+              (progDesc "Validates daicker file")
+          )
+        <> command
+          "run"
+          ( info
+              ( run
+                  <$> fileOpt
+                  <*> argument str (metavar "FUNCTION")
+                  <*> many (argument str (metavar "ARGS"))
+              )
+              (progDesc "Runs the specific function." <> noIntersperse)
+          )
+        <> command
+          "eval"
+          ( info
+              ( eval
+                  <$> fileOpt
+                  <*> argument str (metavar "FUNCTION")
+                  <*> many (argument str (metavar "ARGS"))
+              )
+              (progDesc "Evaluates the specific function and prints the return value to stdout." <> noIntersperse)
+          )
+    )
+
+fileOpt :: Parser String
+fileOpt =
+  strOption
+    ( long "file"
+        <> short 'f'
+        <> metavar "FILENAME"
+        <> value "main.daic"
+    )
+
+check :: String -> IO ()
+check fileName = do
+  res <- runExceptT $ validate fileName
+  case res of
+    Right _ -> hPutStrLn stderr "The module is valid!"
+    Left es -> hPutStrLn stderr $ staticErrorListPretty es
+
+run :: String -> String -> [Text] -> IO ()
+run fileName funcName args = do
+  res <- runExceptT (run' fileName funcName args)
+  case res of
+    Left e -> hExitWithCodeErrors stderr e
+    Right e -> withDevNull (`hExitWithExpr` e)
+
+eval :: String -> String -> [Text] -> IO ()
+eval fileName funcName args = do
+  res <- runExceptT (run' fileName funcName args)
+  case res of
+    Left e -> hExitWithCodeErrors stderr e
+    Right e -> hExitWithExpr stdout e
 
 initialize :: IO ()
 initialize = do
@@ -43,22 +112,22 @@ readModule fileName = do
   (m, _) <- liftEither $ mapLeft StaticE $ parse pModule fileName src
   pure m
 
-run :: String -> String -> [Text] -> ExceptT (CodeError Span) IO (Expr Span)
-run fileName funcName args = do
+run' :: String -> String -> [Text] -> ExceptT (CodeError Span) IO (Expr Span)
+run' fileName funcName args = do
   src <- liftIO $ T.readFile fileName
   (m@(_ :< Module is _ ss), tokens) <- liftEither $ mapLeft StaticE $ parse pModule fileName src
   env <- withExceptT StaticE $ loadEnvironment m
   e <- case findExpr env funcName of
     Nothing -> throwError $ RuntimeE $ RuntimeError ("not found: " <> funcName) (mkSpan "command-line-function" 1 1 1 1) (ExitFailure 1)
     Just (_, e) -> pure e
-  withExceptT StaticE $ liftEither $ validateModule env m
+  withExceptT StaticE $ liftEither $ V.validateModule env m
   case e of
     (s :< ELambda pms (s' :< e) t) -> do
       hasStdin <- liftIO $ hReady stdin
       input <- liftIO $ if hasStdin then Just <$> B.getContents else pure Nothing
       es <- liftEither $ mapLeft StaticE $ mapM (\(i, arg) -> parseArg ("command-line-argument($" <> show i <> ")") input arg) $ zip [1 ..] args
-      withExceptT RuntimeE $ eval env (s :< ECall (s :< ELambda pms (s' :< e) t) es)
-    _ -> withExceptT RuntimeE $ eval env e
+      withExceptT RuntimeE $ E.eval env (s :< ECall (s :< ELambda pms (s' :< e) t) es)
+    _ -> withExceptT RuntimeE $ E.eval env e
 
 mapLeft :: (a -> c) -> Either a b -> Either c b
 mapLeft f = either (Left . f) Right
